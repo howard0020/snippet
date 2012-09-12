e390c15f9a3ef2262e5e9b57f76f3de942f5cac6
Jay Dong <itzjay3@bitbucket.org>
2012-09-11 21:25:06 -0400

    changed layout

--- lift_basic/src/main/scala/bootstrap/liftweb/Boot.scala
+++ lift_basic/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -34,7 +34,7 @@ class Boot {
     // Use Lift's Mapper ORM to populate the database
     // you don't need to use Mapper to use Lift... use
     // any ORM you want
-    Schemifier.schemify(true, Schemifier.infoF _, User, KeyValuePair,CodeSnippet)
+    Schemifier.schemify(true, Schemifier.infoF _, User, KeyValuePair)
 
     // where to search snippet
     LiftRules.addToPackages("code")
