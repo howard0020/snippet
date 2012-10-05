8e60a67615ea3295a64bb435e4752fced76629b8
Howard <howard0020@yahoo.com>
2012-10-04 22:08:01 -0400

    added some method to return most popular Tags

--- lift_basic/src/main/scala/code/model/SnippetTags.scala
+++ lift_basic/src/main/scala/code/model/SnippetTags.scala
@@ -5,9 +5,15 @@ class SnippetTags extends LongKeyedMapper[SnippetTags] with IdPK {
   def getSingleton = SnippetTags
   object codeSnippet extends MappedLongForeignKey(this, CodeSnippet)
   object tag extends MappedLongForeignKey(this, Tag)
+  
+  
 }
 
 
 object SnippetTags extends SnippetTags 
 	with LongKeyedMetaMapper[SnippetTags]{
+  
+   def getTopTag(count:Int):List[Tag] = Tag.findAllByInsecureSql(
+       "SELECT * FROM Tag t1 JOIN ( select tag from snippettags group by tag order by count(tag) desc LIMIT %d) as t2 on t1.id = t2.tag".format(count),
+       IHaveValidatedThisSQL("Howard","10/4/2012"))
 }
--- lift_basic/src/main/scala/code/snippet/Posts.scala
+++ lift_basic/src/main/scala/code/snippet/Posts.scala
@@ -10,26 +10,28 @@ import net.liftweb.common.Failure
 import net.liftweb.http.SessionVar
 import net.liftweb.http.S
 import scala.xml.NodeSeq
-import code.model.Tag
+import code.model._
 import net.liftweb.mapper.By
 
 class Posts {
   
 	def setparam(tempalte: NodeSeq) = {
+	  
+	SnippetTags.getTopTag(1).foreach(tag => Console.println("===tagName>"+tag.name))
     val tag = S.param("tag")
     tag match {
       case Full(name) => Console.println("=Snippet=====> tag:" + name)
       case Empty => Console.println("=Snippet======> tag:Empty")
       case Failure(msg,_,_) => Console.println("=Snippet======> tag:Empty:Failure"+msg)
     }
-    for{
-      session <- S.session
-      tagStr <- S.param("tag")
-    }{
-      val name = S.attr("namehehe",_.toString)
-      val tag = Tag.find(By(Tag.name,tagStr))
-      Console.println("===attr=>"+name.openOr(""))
-      session.setupComet("PostActor",name,tag)
+    val tagStr = tag.openOr("")
+    val name = S.attr("name",_.toString)
+ 	val tagObj = Tag.find(By(Tag.name,tagStr))
+ 	Console.println("===attr=>"+name.openOr(""))
+ 	S.session match {
+      case Full(session) => session.setupComet("PostActor",name,tagObj)
+      case Empty => S.error("no session!")
+      case Failure(msg,_,_) => S.error("no session!"+msg)
     }
     tempalte
   }
