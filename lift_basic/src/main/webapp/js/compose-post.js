fe27e4beac03176d59e923d3df340deb7fabcea1
Howard <howard0020@yahoo.com>
2012-10-21 00:58:34 -0400

    can now delete editable fields and sort them.

--- lift_basic/src/main/webapp/js/compose-post.js
+++ lift_basic/src/main/webapp/js/compose-post.js
@@ -10,31 +10,25 @@
             var newNum  = new Number(num + 1);      // the numeric ID of the new input field being added
             // create the new element via clone(), and manipulate it's ID using newNum value
             // var newElem = $('#input' + num).clone().attr('id', 'input' + newNum);
-            var newField = $('<br><div class="field-content WYSIWYG-fields" contenteditable="true"></div>');
-            newField.next('.field-content').attr('id','Text' + newNum);
+            var newField = $('<div class="sortable-item"><span class="handle">Drag</span><div class="field-content WYSIWYG-fields" contenteditable="true"></div></div>');
+            newField.attr('id','Text' + newNum);
             $('#Text' + num).parent().append(newField);
             
             Aloha.jQuery('.WYSIWYG-fields').mahalo();
         	Aloha.jQuery('.WYSIWYG-fields').aloha();
-            // manipulate the name/id values of the input inside the new element
-           // newElem.children('textarea').attr('class', 'emailContent').attr('id', 'reminderText' + newNum);
-            //newElem.children('input').attr('class', 'schedule').attr('id', 'runReminderInDays' + newNum);
-            // insert the new element after the last "duplicatable" input field
-            
-            // enable the "remove" button
-            //$('#btnDel').removeAttr('disabled','');
+           
           });
           
             $('#btnAddCode').click(function() {
  
             var num     = $('.field-content').length; // how many "duplicatable" input fields we currently have
             var newNum  = new Number(num + 1);      // the numeric ID of the new input field being added
-            // create the new element via clone(), and manipulate it's ID using newNum value
-            // var newElem = $('#input' + num).clone().attr('id', 'input' + newNum);
-            var newField = $('<br><div class="field-content code-block-fields><textarea"></textarea></div>');
-            newField.next('.field-content').attr('id','Text' + newNum);
+           	//create the html to add
+            var newField = $('<div class="field-content code-block-fields"><textarea></textarea></div>');
+            var editorDiv = newField.attr('id','Text' + newNum);
+            editorDiv.children().attr('id','textArea' + newNum);
             $('#Text' + num).parent().append(newField);
-            var newTextArea = document.getElementById('Text' + newNum);
+            var newTextArea = document.getElementById('textArea' + newNum);
             var newEditor = CodeMirror.fromTextArea(newTextArea, {
     		  lineNumbers: true,
 			  value: "",
@@ -54,14 +48,16 @@
              $('#btnDel').removeAttr('disabled','');
           });
         };
+        
+        //remove the last editor
         self.removeFields = function() {
           $('#btnDel').click(function() {
             var num = $('.field-content').length; // how many "duplicatable" input fields we currently have
             $('#Text' + num).remove();     // remove the last element
             
             // if only one element remains, disable the "remove" button
-           // if (num-1 == 1)
-           //   $('#btnDel').attr('disabled','disabled');
+            if (num-1 == 1)
+           	 $('#btnDel').attr('disabled','disabled');
           });
         };
         //collect form data and submit to server
